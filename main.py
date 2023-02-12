12f313720c8c9e7fbe2ae3ce957aedc5a76268c8
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-02-12 16:26:39 +0800

    Importing algo and Creating Window

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import random
+
+# Importing Sorting Algorithms
+from bubble import BubbleSort
+from insertion import InsertionSort
+from selection import SelectionSort
+from merge import merge_sort
+from quick import QuickSort
+
+
+# Making a window using the Tk widget
+window = tk.Tk()
+window.title('SORTING ALGORITHM VISUALIZER')
+window.geometry('600x450')
+
+
