e9c5188bb1f91717edfb20fa92d5d87d7a11f4ec
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-02-12 18:20:30 +0800

    Merge Sorting Code

--- merge.py
+++ merge.py
@@ -2,7 +2,7 @@
 import time #For time in animation
 
 
-def MergeSort(data, start, mid, end, drawData, timeTick):
+def Mergesort(data, start, mid, end, drawData, timeTick):
     p = start
     q = mid + 1
     tempArray = []
@@ -25,13 +25,13 @@ def MergeSort(data, start, mid, end, drawData, timeTick):
         data[start] = tempArray[p]
         start += 1
 
-def merge_sort(data, start, end, drawData, timeTick):
+def MergeSort(data, start, end, drawData, timeTick):
     if start < end:
         mid = int((start + end) / 2)
-        merge_sort(data, start, mid, drawData, timeTick)
-        merge_sort(data, mid+1, end, drawData, timeTick)
+        MergeSort(data, start, mid, drawData, timeTick)
+        MergeSort(data, mid+1, end, drawData, timeTick)
 
-        MergeSort(data, start, mid, end, drawData, timeTick)
+        Mergesort(data, start, mid, end, drawData, timeTick)
 
         drawData(data, ["#9A9A32" if x >= start and x < mid else "#0000FF" if x == mid 
                         else "#8F8FBC" if x > mid and x <=end else "#DCDC14" for x in range(len(data))])
