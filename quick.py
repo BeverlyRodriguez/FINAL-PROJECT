bc2df283ac86dda1f9079be011eb6e55e40c424a
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-02-12 16:09:19 +0800

    Quick Sort Code

--- quick.py
+++ quick.py
@@ -13,11 +13,11 @@ def partition(data, start, end, drawData, timeTick):
     data[start], data[i-1] = data[i-1], data[start]
     return i-1
 
-def quick_sort(data, start, end, drawData, timeTick):
+def QuickSort(data, start, end, drawData, timeTick):
     if start < end:
         pivot_position = partition(data, start, end, drawData, timeTick)
-        quick_sort(data, start, pivot_position-1, drawData, timeTick)
-        quick_sort(data, pivot_position+1, end, drawData, timeTick)
+        QuickSort(data, start, pivot_position-1, drawData, timeTick)
+        QuickSort(data, pivot_position+1, end, drawData, timeTick)
 
         drawData(data, ["#9A9A32" if x >= start and x < pivot_position else "#0000FF" if x == pivot_position
                         else "#8F8FBC" if x > pivot_position and x <=end else "#DCDC14"  for x in range(len(data))])
