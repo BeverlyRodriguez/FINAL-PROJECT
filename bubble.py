3d926396823ece6961e480c1230abdcda074a46f
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-02-13 01:54:04 +0800

    Bubble Sorting Code Final

--- bubble.py
+++ bubble.py
@@ -7,7 +7,7 @@ def BubbleSort(data, drawData, timeTick):
         for j in range(size-i-1):
             if data[j] > data[j+1]:
                 data[j], data[j+1] = data[j+1], data[j]
-                drawData(data, ["#0000FF" if x == j or x == j+1 else "#DCDC14" for x in range(len(data))] )
+                drawData(data, ["#E066FF" if x == j or x == j+1 else "gray10" for x in range(len(data))] )
                 time.sleep(timeTick)
                 
-    drawData(data, ["#0000FF" for x in range(len(data))])
+    drawData(data, ["#E066FF" for x in range(len(data))])
