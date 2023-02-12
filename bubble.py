90ffb94659603562c955fffb0995ca0ba66a18bc
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-02-12 16:07:54 +0800

    Bubble Sorting Code

--- bubble.py
+++ bubble.py
@@ -1,1 +1,13 @@
 #Bubble Sorting Algorithm
+import time #For time in animation
+
+def BubbleSort(data, drawData, timeTick):
+    size = len(data)
+    for i in range(size-1):
+        for j in range(size-i-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+                drawData(data, ["#0000FF" if x == j or x == j+1 else "#DCDC14" for x in range(len(data))] )
+                time.sleep(timeTick)
+                
+    drawData(data, ["#0000FF" for x in range(len(data))])
