e4255cdacb4620e14ffb5efa574c0fd303c2a5ee
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-02-13 01:44:59 +0800

    Insertion Sorting Code Final

--- insertion.py
+++ insertion.py
@@ -10,7 +10,7 @@ def InsertionSort(data, drawData, timeTick):
             data[k] = data[k-1]
             k -= 1
         data[k] = temp
-        drawData(data, ["#0000FF" if x == k or x == i else "#DCDC14" for x in range(len(data))])
+        drawData(data, ["#E066FF" if x == k or x == i else "gray10" for x in range(len(data))])
         time.sleep(timeTick)
         
-    drawData(data, ["#DCDC14" for x in range(len(data))])
+    drawData(data, ["#E066FF" for x in range(len(data))])
